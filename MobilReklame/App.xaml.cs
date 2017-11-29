f3edccc3c2551db5010ffb75e872d169745cbfeb
Patrick Thomas Ørum <patr421b@edu.easj.dk>
2017-11-29 22:25:31 +0100

    Added menu - and navigation between views

--- MobilReklame/App.xaml.cs
+++ MobilReklame/App.xaml.cs
@@ -66,7 +66,7 @@ namespace MobilReklame
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(OrderOverviewView), e.Arguments);
+                    rootFrame.Navigate(typeof(FrontPage), e.Arguments);
                     //rootFrame.Navigate(typeof(MainPage), e.Arguments);
                 }
                 // Ensure the current window is active
